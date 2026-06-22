a8ac370714dbcb8e015e493ad833a2d43e3b6eb8
Pablo Henrique Nunes <reidorodeio.host@gmail.com>
2026-06-22 12:33:44 -0300

    Tighten empty arena headline

--- resources/views/frontend/arena.blade.php
+++ resources/views/frontend/arena.blade.php
@@ -12,7 +12,7 @@
     >
         <img class="arena-hero__logo" src="{{ asset('assets/images/logo/logorei.png') }}" alt="Rei do Rodeio">
         <span class="arena-hero__eyebrow">Arena do bolao</span>
-        <h1>Nenhum evento no momento.</h1>
+        <h1>Nenhum evento agora.</h1>
         <p>Assim que um novo evento for cadastrado, a arena volta a abrir para o bolao e para a montagem da equipe.</p>
         @guest
         <div class="arena-hero__actions">
